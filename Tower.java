22504397b35deca9637cb79ddce5678ba589e907
Denis <chekmarevdenchik5@gmail.com>
2022-09-12 22:25:49 +0700

    tower.java is done

--- Tower.java
+++ Tower.java
@@ -1,4 +1,19 @@
 public class Tower {
 
-        
+
+    static void hanoi_tower(int cout, char start, char middle, char end){
+        if (cout == 1){
+            System.out.printf("%d from %S to %S\n", cout, start, end);
+            return;
+        }
+        hanoi_tower(cout-1, start, end, middle);
+        System.out.printf("%d from %S to %S\n", cout, start, end);
+        hanoi_tower(cout-1, middle, start, end);
+    }
+
+
+    public static void main(String[] args) {
+        int cout = 3;
+        hanoi_tower(cout, 'A', 'B', 'C');
+    }
 }
